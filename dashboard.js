dab3f8c7013d1c236e193f2d4e33e14243d174e6
ZhaoShop <zhaocute3@gmail.com>
2026-04-19 14:48:17 +0800

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -4,11 +4,16 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore, doc, getDoc, addDoc, collection }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const app=initializeApp({
- apiKey:"AIzaSyATT-pyHuOBD9_lD9ee9i0XR0H6YHFJXrI",
- projectId:"webnizhao"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyATT-pyHuOBD9_lD9ee9i0XR0H6YHFJXrI",
+  authDomain: "webnizhao.firebaseapp.com",
+  projectId: "webnizhao",
+  storageBucket: "webnizhao.firebasestorage.app",
+  messagingSenderId: "562185836577",
+  appId: "1:562185836577:web:ce563d59c0b81d02b06b60"
+};
 
+const app = initializeApp(firebaseConfig);
 const auth=getAuth(app);
 const db=getFirestore(app);
 
