99f711e9f9347c692224e68fbec5e9df985cabb7
ZhaoShop <zhaocute3@gmail.com>
2026-04-19 14:49:05 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,11 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const app=initializeApp({
- apiKey:"AIzaSyATT-pyHuOBD9_lD9ee9i0XR0H6YHFJXrI",
- projectId:"webnizhao"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyATT-pyHuOBD9_lD9ee9i0XR0H6YHFJXrI",
+  authDomain: "webnizhao.firebaseapp.com",
+  projectId: "webnizhao",
+  storageBucket: "webnizhao.firebasestorage.app",
+  messagingSenderId: "562185836577",
+  appId: "1:562185836577:web:ce563d59c0b81d02b06b60"
+};
 
+const app = initializeApp(firebaseConfig);
 const db=getFirestore(app);
 
 // ADD STOCK
