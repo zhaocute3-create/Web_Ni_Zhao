3b355de4782ce3cd3a043069e28afab60de062b8
ZhaoShop <zhaocute3@gmail.com>
2026-04-19 14:46:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ import { getFirestore, doc, setDoc, getDoc }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey:"AIzaSyATT-pyHuOBD9_lD9ee9i0XR0H6YHFJXrI",
-  authDomain:"webnizhao.firebaseapp.com",
-  projectId:"webnizhao"
+  apiKey: "AIzaSyATT-pyHuOBD9_lD9ee9i0XR0H6YHFJXrI",
+  authDomain: "webnizhao.firebaseapp.com",
+  projectId: "webnizhao",
+  storageBucket: "webnizhao.firebasestorage.app",
+  messagingSenderId: "562185836577",
+  appId: "1:562185836577:web:ce563d59c0b81d02b06b60"
 };
 
 const app = initializeApp(firebaseConfig);
