29da3feccc6cb8ffcf0f1016f5469792e33673ec
ZhaoShop <zhaocute3@gmail.com>
2026-04-20 22:18:47 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,17 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc }
+import { getFirestore, collection, addDoc, getDocs, doc, setDoc }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyATT-pyHuOBD9_lD9ee9i0XR0H6YHFJXrI",
-  authDomain: "webnizhao.firebaseapp.com",
-  projectId: "webnizhao",
-  storageBucket: "webnizhao.firebasestorage.app",
-  messagingSenderId: "562185836577",
-  appId: "1:562185836577:web:ce563d59c0b81d02b06b60"
-};
+const app=initializeApp({
+ apiKey:"PASTE_API_KEY",
+ projectId:"YOUR_PROJECT"
+});
 
-const app=initializeApp(firebaseConfig);
 const db=getFirestore(app);
 
 // ADD STOCK
@@ -25,61 +20,23 @@ window.addStock=async()=>{
  alert("Stock added");
 };
 
-// ADD COINS
-window.addCoins=async()=>{
- const users=await getDocs(collection(db,"users"));
-
- users.forEach(async d=>{
-   if(d.data().uid==uid.value){
-     await updateDoc(doc(db,"users",d.id),{
-       balance:d.data().balance + Number(coins.value)
-     });
-   }
+// CREATE KEY
+window.genKey=async()=>{
+ await setDoc(doc(db,"keys",newKey.value),{
+   used:false
  });
- alert("Coins added");
+ alert("Key created");
+ load();
 };
 
-// LOAD DEPOSITS
+// LOAD KEYS
 async function load(){
- const q=await getDocs(collection(db,"deposits"));
+ const q=await getDocs(collection(db,"keys"));
  let h="";
-
  q.forEach(d=>{
-   const x=d.data();
-
-   h+=`
-   <div>
-   <p>${x.amount}</p>
-   <button onclick="approve('${d.id}','${x.userId}',${x.amount})">Approve</button>
-   <button onclick="reject('${d.id}')">Reject</button>
-   </div>`;
+   h+=`<p>${d.id} | used: ${d.data().used}</p>`;
  });
-
- list.innerHTML=h;
+ keys.innerHTML=h;
 }
-
-// APPROVE
-window.approve=async(id,uid,amt)=>{
- const users=await getDocs(collection(db,"users"));
-
- users.forEach(async d=>{
-   if(d.id==uid){
-     await updateDoc(doc(db,"users",uid),{
-       balance:d.data().balance + amt
-     });
-   }
- });
-
- await deleteDoc(doc(db,"deposits",id));
- alert("Approved");
- load();
-};
-
-// REJECT
-window.reject=async(id)=>{
- await deleteDoc(doc(db,"deposits",id));
- alert("Rejected");
- load();
-};
 
 load();
