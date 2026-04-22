84f9de96f3d9d516416c939e901452a4ae663fe1
ZhaoShop <zhaocute3@gmail.com>
2026-04-22 18:57:39 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
-// firebase.js (npm version)
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// firebase.js (FINAL WORKING - NO BUILD)
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyATT-pyHuOBD9_lD9ee9i0XR0H6YHFJXrI",
   authDomain: "webnizhao.firebaseapp.com",
   projectId: "webnizhao",
-  storageBucket: "webnizhao.appspot.com", // 🔥 FIX
+  storageBucket: "webnizhao.appspot.com", // FIXED
   messagingSenderId: "562185836577",
   appId: "1:562185836577:web:ce563d59c0b81d02b06b60"
 };
